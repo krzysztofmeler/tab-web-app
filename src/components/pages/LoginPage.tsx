211bc753026e7b5341b6accfabe4d546e286f116
krzysztofmeler <krzysztofmelerqwerty@gmail.com>
2024-06-15 11:26:32 +0200

    added sign in page logic, not working yet, probably because of backend

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,10 +1,62 @@
-import { FC } from 'react';
+import {FC, useContext, useState} from 'react';
 import {Link} from "react-router-dom";
+import {TextInput} from "../forms/TextInput";
+import {jsSubmit} from "../../utils/js-submit";
+import {fetch} from "../../hooks/useRequest.hook";
+import {AuthContext} from "../../AuthContextType";
 
-const LoginPage: FC = () => <>
-  <h2>Login</h2>
-  <p>or</p>
-  <Link to={'/sign-up'}>Create new account</Link>
-</>;
+const LoginPage: FC = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [processing, setProcessing] = useState(false);
+  const [error, setError] = useState<unknown>(null);
+  const [success, setSuccess] = useState(false);
+
+  const {update: setAuthData} = useContext(AuthContext);
+
+
+  const signIn = async () => {
+    setProcessing(true);
+
+    try {
+      const response = await fetch.get('../user', {
+        auth: {
+          password,
+          username: email,
+        }
+      });
+
+      if (response.status === 200) {
+        setAuthData({ password, email });
+        setSuccess(true);
+      }
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    }
+
+    setProcessing(false);
+  };
+
+  return (<>
+    <h2>Login</h2>
+
+
+    <TextInput value={email} updateValue={setEmail} label="E-mail" />
+    <TextInput
+      value={password}
+      updateValue={setPassword}
+      label="Password"
+    />
+    <button type="button" onClick={jsSubmit(signIn)}>
+      Sign in
+    </button>
+
+    <p>or</p>
+    <Link to={'/sign-up'}>Create new account</Link>
+  </>);
+}
 
 export { LoginPage };
