357f3208cc2d5309db5a5f058a3112f8aa5ddc0f
krzysztofmeler <krzysztofmelerqwerty@gmail.com>
2024-06-16 14:37:56 +0200

    added redirect to login on administrative function page

--- src/components/pages/AdministrativeFunctionsPage.tsx
+++ src/components/pages/AdministrativeFunctionsPage.tsx
@@ -1,10 +1,29 @@
-import { FC } from 'react';
+import {FC, useContext, useEffect} from 'react';
 import {Link} from "react-router-dom";
+import {useNavigate} from "react-router";
+import {AuthContext} from "../../AuthContextType";
 
-const AdministrativeFunctionsPage: FC = () => <>
-  <h2>Administrative functions</h2>
+const AdministrativeFunctionsPage: FC = () => {
 
-  <Link to={'/administration/tags'}>Tags</Link>
-</>;
+  const navigate = useNavigate()
+
+  const { data: authData } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (authData === null) {
+      navigate('/sign-in');
+    }
+  })
+
+  if (authData === null) {
+    return <>You must login first</>
+  }
+
+  return <>
+    <h2>Administrative functions</h2>
+
+    <Link to={'/administration/tags'}>Tags</Link>
+  </>;
+}
 
 export { AdministrativeFunctionsPage };
