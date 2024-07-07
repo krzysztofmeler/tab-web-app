e1dcf0ea1d7f2abcdd11da82b9b01fc161bba245
krzysztofmeler <krzysztofmelerqwerty@gmail.com>
2024-07-07 12:51:36 +0200

    added styles to sign up page

--- src/components/pages/SignUpPage.tsx
+++ src/components/pages/SignUpPage.tsx
@@ -1,10 +1,11 @@
 import { FC, useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
-import { TextInput } from '../forms/TextInput';
 import { jsSubmit } from '../../utils/js-submit';
 import { fetch } from '../../hooks/useRequest.hook';
 import { AuthContext, Role } from '../../AuthContextType';
 import { AuthorizationHeaderFromEmailAndPassword } from '../../utils/auth';
+import {Button, Card, Checkbox, Space, Text, TextInput} from "@mantine/core";
+import {Link} from "react-router-dom";
 
 const SignUpPage: FC = () => {
     const [email, setEmail] = useState('');
@@ -56,18 +57,49 @@ const SignUpPage: FC = () => {
     }, [success]);
 
     return (
-        <>
-            Sign Up page
-            <TextInput value={email} updateValue={setEmail} label="E-mail" />
-            <TextInput
-              value={password}
-              updateValue={setPassword}
-              label="Password"
-            />
-            <button type="button" onClick={jsSubmit(signUp)}>
-                Sign up
-            </button>
-        </>
+      <Card
+        style={{ boxShadow: '0 0 20px 0 rgba(0, 0, 0, 0.15)' }}
+        maw={600}
+        mx={'auto'}
+        my={50}
+        p={30}
+
+      >
+
+          <Text component={'h2'} size={'xl'}>Sign up</Text>
+
+          <Space h={20}/>
+
+          <TextInput value={email} onChange={e => setEmail(e.target.value)} label="E-mail" />
+
+          <Space h={10} />
+
+          <TextInput
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            label="Password"
+          />
+
+          <Space h={20} />
+
+          <Checkbox label={'I accept terms and conditions of services'} />
+
+          <Space h={20} />
+
+
+          <Button
+            disabled={processing}
+            type="button"
+            onClick={jsSubmit(signUp)}
+          >
+              Create account
+          </Button>
+
+          <Space h={20} />
+          <Text mx={'auto'} c={'gray'}>or</Text>
+          <Space h={20} />
+          <Button variant={'light'} component={Link} to={'/sign-in'}>Sign in with existing account</Button>
+      </Card>
     );
 };
 
