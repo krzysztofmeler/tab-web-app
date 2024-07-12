a109c361f3a847ade7e89aeea695365f54388257
krzysztofmeler <krzysztofmelerqwerty@gmail.com>
2024-07-12 21:34:50 +0200

    better styling of recipe list, added search by name and category

--- src/components/pages/RecipeListPage.tsx
+++ src/components/pages/RecipeListPage.tsx
@@ -1,9 +1,10 @@
-import React, { FC, useState } from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { useAsyncEffect } from '../../hooks/useAsyncEffect.hook';
 import { fetch } from '../../hooks/useRequest.hook';
 import { useAuthContextRedirect } from '../../hooks/useAuthContextRedirect.hook';
 import { Recipe } from '../../types/Recipe';
+import {Button, Card, Flex, MultiSelect, Select, Space, Text, TextInput} from "@mantine/core";
 
 const RecipeListPage: FC = () => {
     const { data: authData } = useAuthContextRedirect();
@@ -22,27 +23,58 @@ const RecipeListPage: FC = () => {
         }
     }, []);
 
-    return (
-        <>
-            <h2>Recipe list</h2>
+    const [searchString, setSearchString] = useState('')
 
-            <ul>
-                {recipes.map((recipe) => (
-                    <Link
-                      key={recipe.id}
-                      to={`/recipe/${recipe.id}/${encodeURIComponent(
-                            recipe.name,
-                        )}`}
-                    >
-                        <p>{recipe.name}</p>
-                        <p>
-                            {recipe.description.slice(0, 120)}
-                            {recipe.description.length > 120 ? '...' : ''}
-                        </p>
-                    </Link>
+    const categories = new Set<string>();
+
+    recipes.forEach(recipe => {
+        categories.add(recipe.categories[0]);
+    });
+
+      const categories2 = [...categories];
+
+      const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+      const filteredRecipes = recipes.filter(recipe => {
+          if ( searchString.length && recipe.name.indexOf(searchString) === -1)
+              return false;
+
+          if (selectedCategories.length) {
+              return selectedCategories.includes(recipe.categories[0]);
+          } else {
+              return true;
+          }
+      })
+
+    return (
+        <Flex maw={1200} my={50} mx={'auto'} gap={20} direction={'column'}>
+            <Text component={'h2'}>Recipe list</Text>
+
+            <Flex>
+                <TextInput label={'Search'} value={searchString} onChange={e => setSearchString(e.target.value)} />
+                <Space w={20} />
+                <MultiSelect w={340} onChange={setSelectedCategories} label={'Categories'} multiple data={categories2} value={selectedCategories} />
+            </Flex>
+
+                {filteredRecipes.map((recipe) => (
+                        <Card style={{boxShadow: '0 0 5px 0 #ddd'}}>
+                            <Text size={'lg'} style={{ textDecoration: '' }}>
+                                { recipe.name }
+                            </Text>
+                            <Text size={'xs'}>
+                                {recipe.description}
+                            </Text>
+
+                            <Space h={5} />
+
+                            <Text size={'xs'} c={'#666'}>{recipe.steps.length} step{recipe.steps.length > 1 && 's'} | Category: {recipe.categories[0]}</Text>
+
+                            <Space h={10} />
+
+                            <Button maw={120} variant={'light'} component={Link} to={`/recipe/${recipe.id}/${encodeURIComponent(recipe.name)}`}>Show</Button>
+                        </Card>
                 ))}
-            </ul>
-        </>
+        </Flex>
     );
 };
 
